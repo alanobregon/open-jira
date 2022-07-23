178219d53d3e0c5c8a54d82570ff756fd13eebe7
Alan Obregon <alan.obregon49@gmail.com>
2022-07-23 17:00:46 -0300

    Implementados Entries Context, Provider y Reducer

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,12 +1,33 @@
-import { FC, ReactNode, useReducer } from "react";
+import { Entry } from "../../interfaces";
 import { EntriesContext, EntriesReducer } from "./";
+import { FC, ReactNode, useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export interface EntriesState {
-  entries: [];
+  entries: Entry[];
 }
 
-const UI_INITIAL_STATE: EntriesState = {
-  entries: [],
+const ENTRIES_INITIAL_STATE: EntriesState = {
+  entries: [
+    {
+      _id: uuidv4(), 
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, corporis!", 
+      createdAt: Date.now(), 
+      status: "pending" 
+    },
+    {
+      _id: uuidv4(), 
+      description: "Laboris et anim quis nostrud non veniam Lorem sit consequat.", 
+      createdAt: Date.now() - 1000000, 
+      status: "in-progress" 
+    },
+    {
+      _id: uuidv4(), 
+      description: "Velit eu deserunt commodo sunt ullamco officia commodo sunt excepteur.", 
+      createdAt: Date.now() - 100000, 
+      status: "finished" 
+    },
+  ],
 }
 
 interface Props {
@@ -15,7 +36,7 @@ interface Props {
 
 export const EntriesProvider: FC<Props> = (props) => {
   const { children } = props;
-  const [state, dispatch] = useReducer(EntriesReducer, UI_INITIAL_STATE);
+  const [state, dispatch] = useReducer(EntriesReducer, ENTRIES_INITIAL_STATE);
 
   return (
     <EntriesContext.Provider value={{
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,15 +5,18 @@ import CssBaseline from '@mui/material/CssBaseline';
 
 import { light, dark } from '../themes';
 import { UIProvider } from '../context/ui';
+import { EntriesProvider } from '../context/entries';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UIProvider>
-      <ThemeProvider theme={dark}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </UIProvider>
+    <EntriesProvider>
+      <UIProvider>
+        <ThemeProvider theme={dark}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </UIProvider>
+    </EntriesProvider>
   )
 }
 
