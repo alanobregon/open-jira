9481ef755fc461679dc13121b82fd45b29b05f3a
Alan Obregon <alan.obregon49@gmail.com>
2022-07-23 19:18:39 -0300

    Agregado NewEntry al estado global.

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -7,9 +7,11 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 
 import MenuOutlined from "@mui/icons-material/MenuOutlined";
+import AddCircleOutline from "@mui/icons-material/AddCircleOutline";
+import Button from "@mui/material/Button";
 
 export const Header = () => {
-  const {openSidebar} = useContext(UIContext);
+  const {openSidebar, openEntriesModal} = useContext(UIContext);
 
   return (
     <AppBar position="sticky">
@@ -17,9 +19,17 @@ export const Header = () => {
         <IconButton size="large" edge="start" onClick={openSidebar}>
           <MenuOutlined />
         </IconButton>
-        <Typography variant="h6">
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
           Open Jira
         </Typography>
+        <Button
+          size="medium" 
+          color="inherit" 
+          startIcon={<AddCircleOutline />}
+          onClick={openEntriesModal}
+        >
+          Agregar tarea
+        </Button>
       </Toolbar>
     </AppBar>
   )
