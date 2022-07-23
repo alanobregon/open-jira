318e2fc0b1d1d9ae9bb1a35ea96518b0b057f483
Alan Obregón <alan.obregon@neoris.com>
2022-07-23 15:36:02 -0300

    Arreglo warn de full refresh

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,7 +1,17 @@
-import { InboxOutlined, MailOutline } from "@mui/icons-material";
-import { Box, Divider, Drawer, List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material"
 import { useContext } from "react";
 import { UIContext } from "../../context/ui";
+
+import InboxOutlined from "@mui/icons-material/InboxOutlined";
+import MailOutline from "@mui/icons-material/MailOutline";
+
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import Drawer from "@mui/material/Drawer";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import Typography from "@mui/material/Typography";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 
 const items: string[] = [
   "Inbox",
