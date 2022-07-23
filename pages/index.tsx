21a096326b9a2d1f22ffbb7352f6543806d53406
Alan Obregon <alan.obregon49@gmail.com>
2022-07-23 16:20:25 -0300

    Estructura IndexPage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,35 @@
 import type { NextPage } from "next";
 import { Layout } from "../components/layouts";
 
-import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardHeader";
+import CardHeader from "@mui/material/CardContent";
 
 const HomePage: NextPage = () => {
   return (
     <Layout>
-      <Typography variant='h1' color={`primary`}>
-        Hola mundo
-      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title={`Pendientes`} />
+            <CardContent>
+              {/* Agregar nuevas entradas */}
+              {/* Listar entradas */}
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title={`En Progreso`} />
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title={`Completadas`} />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
