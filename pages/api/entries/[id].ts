467a17f3611ad2f09ceb792c8ae7c46e7963c7a1
Alan Obregón <alan.obregon@neoris.com>
2022-08-02 18:22:08 -0300

    devolver entry en base al id

--- pages/api/entries/[id].ts
+++ pages/api/entries/[id].ts
@@ -3,9 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { db } from "../../../database";
 import { Entry, IEntry } from "../../../database/models";
 
-type Data = 
-| { message: string; }
-| IEntry
+type Data = { message: string } | IEntry;
 
 export default function handler(
   req: NextApiRequest,
@@ -18,14 +16,32 @@ export default function handler(
   }
 
   switch (req.method) {
+    case "GET":
+      return getEntry(req, res);
     case "PUT":
       return updateEntry(req, res);
     default:
       return res.status(400).json({
-        message: "El id no es valido",
+        message: `No existe el metodo ${req.method}`,
       });
   }
 }
+
+const getEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { id } = req.query;
+  await db.connect();
+
+  const entry = await Entry.findById(id);
+  await db.disconnect();
+
+  if (!entry) {
+    return res.status(400).json({
+      message: "No hay entrada con ese id.",
+    });
+  }
+  
+  res.status(200).json(entry);
+};
 
 const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   const { id } = req.query;
@@ -50,13 +66,13 @@ const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
       { description, status },
       { runValidators: true, new: true }
     );
-  
+
     await db.disconnect();
-    res.status(200).json(updatedEntry!) 
+    res.status(200).json(updatedEntry!);
   } catch (error: any) {
     await db.disconnect();
     res.status(400).json({
-      message: error.errors.status.message
-    })
+      message: error.errors.status.message,
+    });
   }
 };
